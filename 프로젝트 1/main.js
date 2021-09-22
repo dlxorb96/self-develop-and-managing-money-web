0a8fee53b23cf6a7a24ab83fa01de93985dbb013
dlxorb96 <80591023+dlxorb96@users.noreply.github.com>
2021-09-22 17:49:07 +0900

    추가사항
    +하나 만들었음. 더 조절해야함

--- 프로젝트 1/main.js
+++ 프로젝트 1/main.js
@@ -3,20 +3,29 @@
 const firstNavBar = document.querySelector('.nav__bar li:nth-child(1)')
 const secondNavBar = document.querySelector('.nav__bar li:nth-child(2)')
 const thirdNavBar = document.querySelector('.nav__bar li:nth-child(3)')
+const plusButton = document.querySelector('#plus')
+
 
 function firstClickEvent(e){
   e.preventDefault()
   firstNavBar.classList.add('clicked')
+  secondNavBar.classList.remove('clicked')
+  thirdNavBar.classList.remove('clicked')
+  
 }
 function secondClickEvent(e){
   e.preventDefault()
   secondNavBar.classList.add('clicked')
+  firstNavBar.classList.remove('clicked')
+  thirdNavBar.classList.remove('clicked')
 
 
 }
 function thirdClickEvent(e){
   e.preventDefault()
   thirdNavBar.classList.add('clicked')
+  firstNavBar.classList.remove('clicked')
+  secondNavBar.classList.remove('clicked')
 
 }
 
